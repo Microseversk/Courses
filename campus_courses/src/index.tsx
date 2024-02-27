f7db7220f8925aed377fd4a464666b1bdc80573c
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-02-27 21:07:55 +0700

    Init project

---
+++ campus_courses/src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <>asdasdasdasd</>
+    </React.StrictMode>
+);
