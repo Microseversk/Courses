ccf473ba02e61248b8b038508c35c5230592d339
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-03-09 02:33:54 +0700

    Fix student list

--- campus_courses/src/components/pages/courseDetailsPage/memberTabsDetails/StudentsList.tsx
+++ campus_courses/src/components/pages/courseDetailsPage/memberTabsDetails/StudentsList.tsx
@@ -1,71 +1,92 @@
-import {AlertLink, Button, Col, ListGroup, ListGroupItem, Row} from "react-bootstrap";
-import {IStudent} from "../../../../types/response.types";
+import {
+	AlertLink,
+	Button,
+	Col,
+	ListGroup,
+	ListGroupItem,
+	Row,
+} from 'react-bootstrap'
+import { useTypedSelector } from '../../../../hooks/useTypedSelector'
+import { IStudent } from '../../../../types/response.types'
 
-interface IStudentsListProps{
-    students: IStudent[],
+interface IStudentsListProps {
+	students: IStudent[]
 }
-export function StudentsList(props: IStudentsListProps){
-    return(
-        <ListGroup>
-            {props.students.map((student) => (
-                <ListGroupItem key={student.id} className={'border-0 border-bottom'}>
-                    <Row>
-                        <Col sm={12} md={4}>
-                            <div>{student.name}</div>
-                            <div>
-                                <span className={'text-muted'}>Статус - </span>
-                                {
-                                    student.status === 'Accepted' ?
-                                        <span className={'text-success'}>принят в группу</span>
-                                        :
-                                        student.status === 'Declined' ?
-                                            <span className={'text-danger'}>отклонен</span>
-                                            :
-                                            <span className={'text-primary'}>в очереди</span>
-                                }
-                            </div>
-                            <div className={'text-muted'}>{student.email}</div>
-                        </Col>
-                        {student.status === 'Accepted' &&
-                            <>
-                                <Col sm={12} md={4} className={' mt-2 mt-md-0'}>
-                                    <AlertLink className={'link-primary fw-normal'}>Промежуточная аттестация
-                                        -</AlertLink>
-                                    {student.midtermResult === 'Passed' ?
-                                        <span className={'ms-1 badge bg-success'}>Пройдена</span>
-                                        :
-                                        student.midtermResult === 'Failed' ?
-                                            <span className={'ms-1 badge bg-danger'}>Провалена</span>
-                                            :
-                                            <span className={'ms-1 badge bg-secondary'}>Нет отметки</span>
-                                    }
-
-
-                                </Col>
-                                <Col sm={12} md={4}>
-                                    <AlertLink className={'link-primary fw-normal'}>Финальная аттестация
-                                        -</AlertLink>
-                                    {student.finalResult === 'Passed' ?
-                                        <span className={'ms-1 badge bg-success'}>Пройдена</span>
-                                        :
-                                        student.finalResult === 'Failed' ?
-                                            <span className={'ms-1 badge bg-danger'}>Провалена</span>
-                                            :
-                                            <span className={'ms-1 badge bg-secondary'}>Нет отметки</span>
-                                    }
-                                </Col>
-                            </>
-                        }
-                        {student.status === 'InQueue' &&
-                            <Col sm={12} md={8} className={'d-flex align-content-stretch justify-content-start justify-content-md-end'}>
-                                <Button>Принять</Button>
-                                <Button className={'ms-3 btn-danger'}>Отклонить заявку</Button>
-                            </Col>
-                        }
-
-                    </Row>
-                </ListGroupItem>
-            ))}
-        </ListGroup>
-    )
+export function StudentsList(props: IStudentsListProps) {
+	const { course, userCourseRole } = useTypedSelector(
+		state => state.openedCourse
+	)
+	console.log(course, userCourseRole)
+	return (
+		<ListGroup>
+			{props.students.map(student => (
+				<ListGroupItem key={student.id} className={'border-0 border-bottom'}>
+					<Row>
+						<Col sm={12} md={4}>
+							<div>{student.name}</div>
+							<div>
+								<span className={'text-muted'}>Статус - </span>
+								{student.status === 'Accepted' ? (
+									<span className={'text-success'}>принят в группу</span>
+								) : student.status === 'Declined' ? (
+									<span className={'text-danger'}>отклонен</span>
+								) : (
+									<span className={'text-primary'}>в очереди</span>
+								)}
+							</div>
+							<div className={'text-muted'}>{student.email}</div>
+						</Col>
+						{student.status === 'Accepted' && student.midtermResult && (
+							<>
+								<Col sm={12} md={4} className={' mt-2 mt-md-0'}>
+									<AlertLink className={'link-primary fw-normal'}>
+										Промежуточная аттестация -
+									</AlertLink>
+									{student.midtermResult === 'Passed' ? (
+										<span className={'ms-1 badge bg-success'}>Пройдена</span>
+									) : student.midtermResult === 'Failed' ? (
+										<span className={'ms-1 badge bg-danger'}>Провалена</span>
+									) : student.midtermResult === 'NotDefined' ? (
+										<span className={'ms-1 badge bg-secondary'}>
+											Нет отметки
+										</span>
+									) : (
+										<></>
+									)}
+								</Col>
+								<Col sm={12} md={4}>
+									<AlertLink className={'link-primary fw-normal'}>
+										Финальная аттестация -
+									</AlertLink>
+									{student.finalResult === 'Passed' ? (
+										<span className={'ms-1 badge bg-success'}>Пройдена</span>
+									) : student.finalResult === 'Failed' ? (
+										<span className={'ms-1 badge bg-danger'}>Провалена</span>
+									) : student.finalResult === 'NotDefined' ? (
+										<span className={'ms-1 badge bg-secondary'}>
+											Нет отметки
+										</span>
+									) : (
+										<></>
+									)}
+								</Col>
+							</>
+						)}
+						{student.status === 'InQueue' && (
+							<Col
+								sm={12}
+								md={8}
+								className={
+									'd-flex align-content-stretch justify-content-start justify-content-md-end'
+								}
+							>
+								<Button>Принять</Button>
+								<Button className={'ms-3 btn-danger'}>Отклонить заявку</Button>
+							</Col>
+						)}
+					</Row>
+				</ListGroupItem>
+			))}
+		</ListGroup>
+	)
 }
