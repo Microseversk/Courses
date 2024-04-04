97f458372b4ca9677a34e9415fa337e003901b21
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-04-04 23:42:01 +0700

    Update size not found image

--- campus_courses/src/components/pages/notFoundPage/NotFoundPage.tsx
+++ campus_courses/src/components/pages/notFoundPage/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap'
 export default function NotFoundPage() {
 	return (
 		<Container className={'d-flex justify-content-center '}>
-			<img style={{ maxWidth: '50%' }} src='/images/not_found_image.jpg' />
+			<img style={{ maxWidth: '60%' }} src='/images/not_found_image.jpg' />
 		</Container>
 	)
 }
