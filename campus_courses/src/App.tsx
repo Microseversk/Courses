cda39b440179052ef20ac5d7b30d76176367e9f9
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-04-12 20:01:43 +0700

    init i18next

--- campus_courses/src/App.tsx
+++ campus_courses/src/App.tsx
@@ -1,6 +1,17 @@
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import '../node_modules/mdb-react-ui-kit/dist/css/mdb.min.css'
 import { Router } from './components/router/Router'
+import { store } from './store/store'
 
 export default function App() {
-	return <Router />
+	return (
+		<Provider store={store}>
+			<Toaster position='top-center' toastOptions={{ duration: 1000 }} />
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</Provider>
+	)
 }
--- campus_courses/src/index.tsx
+++ campus_courses/src/index.tsx
@@ -1,18 +1,13 @@
+import { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import { Toaster } from 'react-hot-toast'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App'
-import { store } from './store/store'
+import './i18n'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-	<Provider store={store}>
-		<Toaster position='top-center' toastOptions={{ duration: 1000 }} />
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</Provider>
+	<Suspense>
+		<App />
+	</Suspense>
 )
