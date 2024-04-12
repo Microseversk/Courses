8086db7f983b2164b64d67fa84adf091f78f3d3b
Alexander Osipov <alexander.osipov.work@gmail.com>
2024-04-12 18:21:56 +0700

    Fix change course status

--- campus_courses/src/components/pages/courseDetailsPage/modals/ChangeStatusModal.tsx
+++ campus_courses/src/components/pages/courseDetailsPage/modals/ChangeStatusModal.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from 'react'
 import { Button, Form, FormCheck, Modal, ModalBody, ModalFooter, ModalHeader } from 'react-bootstrap'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useToastMutate } from '../../../../hooks/useToastMutate'
 import { useTypedSelector } from '../../../../hooks/useTypedSelector'
 import { useEditCourseStatusMutation } from '../../../../store/api/coursesApi'
 import { ICourseEditStatus } from '../../../../types/request.types'
+import { ButtonCustom } from '../../../shared/ButtonCustom'
 
 interface IChangeStatusModalProps {
 	isShow: boolean
@@ -11,29 +13,37 @@ interface IChangeStatusModalProps {
 }
 
 export function ChangeStatusModal(props: IChangeStatusModalProps) {
-	const [editStatus, { isSuccess, isError }] = useEditCourseStatusMutation()
+	const [editStatus, { isSuccess, isError, isLoading }] = useEditCourseStatusMutation()
 	const { status, id } = useTypedSelector(state => state.openedCourse.course!)
 
 	const onChangeCourseStatus: SubmitHandler<ICourseEditStatus> = data => {
 		editStatus(data)
-		props.onHide()
 	}
 
-	const { register, handleSubmit, reset } = useForm<ICourseEditStatus>({
+	const { register, handleSubmit, setValue, reset } = useForm<ICourseEditStatus>({
 		defaultValues: {
 			courseId: id,
+			status: status,
 		},
 	})
 
 	useToastMutate(isSuccess, isError, 'Статус изменён')
 
-	const onModalHide = () => {
-		reset()
-		props.onHide()
-	}
+	setValue('status', status)
+	useEffect(() => {
+		if (!isLoading) {
+			props.onHide()
+		}
+	}, [isLoading])
 
 	return (
-		<Modal show={props.isShow} onHide={onModalHide} size={'lg'}>
+		<Modal
+			show={props.isShow}
+			onHide={() => {
+				props.onHide()
+				reset()
+			}}
+			size={'lg'}>
 			<ModalHeader closeButton>Изменение статуса курса</ModalHeader>
 			<ModalBody>
 				<Form
@@ -79,12 +89,12 @@ export function ChangeStatusModal(props: IChangeStatusModalProps) {
 				</Form>
 			</ModalBody>
 			<ModalFooter>
-				<Button className={'btn-secondary'} onClick={onModalHide}>
+				<Button className={'btn-secondary'} onClick={props.onHide}>
 					Отмена
 				</Button>
-				<Button type={'submit'} form={'formChangeStatus'}>
+				<ButtonCustom type={'submit'} form={'formChangeStatus'} isLoading={isLoading} text={'Сохранить'}>
 					Сохранить
-				</Button>
+				</ButtonCustom>
 			</ModalFooter>
 		</Modal>
 	)
